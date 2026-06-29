d69d703a202be074010fe2102a9f65cdda787a42
Petr Hechtberger <hecht@MacBook-Air-uzivatela-Petr.local>
2026-06-29 21:07:40 +0200

    POZOR/varovania: biely text na cervenom pozadi v tmavom rezime (oprava kontrastu); svetly rezim bez zmeny

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='alpeadria-v17';
-const ASSETS=['./','./index.html','./data.js?v=17','./manifest.json'];
+const C='alpeadria-v18';
+const ASSETS=['./','./index.html','./data.js?v=18','./manifest.json'];
 
 function netRace(req,ms){
   return Promise.race([
